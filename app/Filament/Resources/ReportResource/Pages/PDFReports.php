e8ee1d020ae01b3f85f25776f4cc506761c1377a
dunnokim020 <kimraymondamador@gmail.com>
2025-09-12 02:30:21 +0800

    RENAME

--- app/Filament/Resources/ReportResource/Pages/PDFReports.php
+++ app/Filament/Resources/ReportResource/Pages/PDFReports.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\ReportResource\Pages;
 use App\Exports\ReportsExport;
 use App\Filament\Resources\ReportResource;
 use Filament\Actions;
+use Filament\Forms;
 use Filament\Resources\Pages\Page;
 use Maatwebsite\Excel\Facades\Excel;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -13,7 +14,6 @@ class ListReports extends Page
 {
     protected static string $resource = ReportResource::class;
 
-    // custom blade, renders only widgets (no table)
     protected static string $view = 'filament.resources.report-resource.pages.list-reports';
 
     protected function getHeaderActions(): array
@@ -21,19 +21,22 @@ class ListReports extends Page
         return [
             Actions\Action::make('export_pdf')
                 ->label('Export PDF')
-              //  ->icon('heroicon-o-document-text')
-                ->action('exportPdf'),
+                // open a form/modal to ask for filename
+                ->form([
+                    Forms\Components\TextInput::make('filename')
+                        ->label('File name')
+                        ->default('reports_' . now()->format('Ymd_His'))
+                        ->required()
+                        ->helperText('Without extension (".pdf" will be added)'),
+                ])
+                ->action(function (array $data): BinaryFileResponse {
+                    $filename = $data['filename'] . '.pdf';
+                    return Excel::download(
+                        new ReportsExport(),
+                        $filename,
+                        \Maatwebsite\Excel\Excel::MPDF
+                    );
+                }),
         ];
-    }
-
-    // public so Livewire / Filament can call it
-    public function exportPdf(): BinaryFileResponse
-    {
-        // Use the PDF driver. You can use MPDF, DOMPDF, or TCPDF depending on which you installed
-        return Excel::download(
-            new ReportsExport(),
-            'reports_' . now()->format('Ymd_His') . '.pdf',
-            \Maatwebsite\Excel\Excel::MPDF
-        );
     }
 }
