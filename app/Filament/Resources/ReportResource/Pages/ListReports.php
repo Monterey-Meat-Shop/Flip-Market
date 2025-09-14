61cd63d096c514ab584e0b88782714c54be77727
dunnokim020 <kimraymondamador@gmail.com>
2025-09-15 00:43:29 +0800

    Update list reports

--- app/Filament/Resources/ReportResource/Pages/ListReports.php
+++ app/Filament/Resources/ReportResource/Pages/ListReports.php
@@ -8,36 +8,54 @@ use Filament\Actions;
 use Filament\Forms;
 use Filament\Resources\Pages\Page;
 use Maatwebsite\Excel\Facades\Excel;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class ListReports extends Page
 {
     protected static string $resource = ReportResource::class;
-
     protected static string $view = 'filament.resources.report-resource.pages.list-reports';
 
     protected function getHeaderActions(): array
     {
         return [
-            Actions\Action::make('export_pdf')
-                ->label('Export PDF')
-                // open a form/modal to ask for filename
+            Actions\Action::make('export_excel')
+                ->label('Export Excel')
+                ->icon('heroicon-o-document-arrow-down')
                 ->form([
                     Forms\Components\TextInput::make('filename')
-                        ->label('File name')
-                        ->default('reports_' . now()->format('Ymd_His'))
+                        ->label('File Name')
+                        ->default('weekly-report_' . now()->format('Ymd_His'))
                         ->required()
-                        ->helperText('Without extension (".pdf" will be added)'),
+                        ->helperText('File will be saved as .xlsx'),
                 ])
-                ->action(function (array $data): BinaryFileResponse {
-                    $filename = $data['filename'] . '.pdf';
-                    return Excel::download(
-                        new ReportsExport(),
-                        $filename,
-                        \Maatwebsite\Excel\Excel::MPDF
+                ->action(fn (array $data): BinaryFileResponse =>
+                    Excel::download(new ReportsExport(), "{$data['filename']}.xlsx")
+                ),
+
+            Actions\Action::make('export_pdf')
+                ->label('Export PDF')
+                ->icon('heroicon-o-printer')
+                ->form([
+                    Forms\Components\TextInput::make('filename')
+                        ->label('File Name')
+                        ->default('weekly-report_' . now()->format('Ymd_His'))
+                        ->required()
+                        ->helperText('File will be saved as .pdf'),
+                ])
+                ->action(function (array $data) {
+                    $export = new ReportsExport();
+
+                    $pdf = Pdf::loadView('pdf.weekly-report', [
+                        'headings' => $export->headings(),
+                        'rows'     => $export->array(),
+                    ])->setPaper('A4', 'portrait');
+
+                    return response()->streamDownload(
+                        fn () => print($pdf->output()),
+                        "{$data['filename']}.pdf"
                     );
                 }),
         ];
     }
-
 }
--- resources/views/filament/resources/report-resource/pages/list-reports.blade.php
+++ resources/views/filament/resources/report-resource/pages/list-reports.blade.php
@@ -1,15 +1,14 @@
 <x-filament::page>
-    <div class="space-y-6">
+    <x-filament::section>
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
             @livewire(\App\Filament\Resources\ReportResource\Widgets\ReportStats::class)
             @livewire(\App\Filament\Resources\ReportResource\Widgets\OrdersStats::class)
             @livewire(\App\Filament\Resources\ReportResource\Widgets\PaymentsStats::class)
             @livewire(\App\Filament\Resources\ReportResource\Widgets\OrdersRealtimeStats::class)
         </div>
+    </x-filament::section>
 
-        {{-- full width chart widget --}}
-        <div class="mt-6">
-            @livewire(\App\Filament\Resources\ReportResource\Widgets\OrdersSalesChart::class)
-        </div>
-    </div>
+    <x-filament::section>
+        @livewire(\App\Filament\Resources\ReportResource\Widgets\OrdersSalesChart::class)
+    </x-filament::section>
 </x-filament::page>
