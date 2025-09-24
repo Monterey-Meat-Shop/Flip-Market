79df7fb69bbb5a3bf966868ddd32cf5ed3308d26
Christian Vacaro <vacaro.267597@gmail.com>
2025-09-24 20:37:01 +0800

    homepage-brand

--- resources/views/livewire/partial/footer.blade.php
+++ resources/views/livewire/partial/footer.blade.php
@@ -3,7 +3,7 @@
     <!-- Grid -->
     <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
       <div class="col-span-full lg:col-span-1">
-        <a class="flex-none text-xl font-semibold text-white" href="#" aria-label="Brand">E-Commerce</a>
+        <a class="flex-none text-xl font-semibold text-white" href="#" aria-label="Brand">Flip Market</a>
       </div>
       <!-- End Col -->
 
@@ -49,7 +49,7 @@
 
     <div class="mt-5 sm:mt-12 grid gap-y-2 sm:gap-y-0 sm:flex sm:justify-between sm:items-center">
       <div class="flex justify-between items-center">
-        <p class="text-sm text-gray-400">© 2024 E-Commerce-rezerdire. All rights reserved.</p>
+        <p class="text-sm text-gray-400">© 2024 Flip Market. All rights reserved.</p>
       </div>
       <!-- End Col -->
 
--- resources/views/livewire/partial/navigation.blade.php
+++ resources/views/livewire/partial/navigation.blade.php
@@ -24,7 +24,7 @@
 
             <a wire:navigate class="font-medium {{ request()->is('/') ? 'text-blue-600' : 'text-gray-500' }} " href="/" aria-current="page">Home</a>
 
-            <a class="font-medium text-gray-500 hover:text-gray-400 py-3 md:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="/brands">
+            <a wire:navigate class ="font-medium {{ request()->is('brands') ? 'text-blue-600' : 'text-gray-500' }}" href="/brands">
               Brands
             </a>
 
