8a818451404e898ed7268d6f454a907913529fbb
dunnokim020 <kimraymondamador@gmail.com>
2025-11-10 02:01:12 +0800

    upd v.2 viteeee

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ command }) => ({
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-    tailwindcss(),
-  ],
-  base: command === 'build' ? '/build/' : '',
-  build: {
-    outDir: 'public/build',
-    manifest: true,
-    emptyOutDir: true,
-  },
-}));
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
