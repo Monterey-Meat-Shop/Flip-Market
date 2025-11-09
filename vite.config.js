cac4f9e00d73d658df0caf8fd3650d94078dc85c
dunnokim020 <kimraymondamador@gmail.com>
2025-11-10 01:42:18 +0800

    dep vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -12,7 +10,20 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    server: {
-        cors: true,
+
+    base: command === 'build' ? '/build/' : '',
+
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
     },
-});
+
+    server: {
+        host: 'localhost',
+        port: 5173,
+        cors: true,
+        strictPort: true,
+        hmr: false, // ✅ disables hot reload on production
+    },
+}));
