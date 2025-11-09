b4d11618d73d0231a2d778f2b46e31681c5a7b47
dunnokim020 <kimraymondamador@gmail.com>
2025-11-10 01:48:34 +0800

    upddd!

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,17 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command }) => ({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-
-    base: command === 'build' ? '/build/' : '',
-
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        emptyOutDir: true,
-    },
-
-    server: {
-        host: 'localhost',
-        port: 5173,
-        cors: true,
-        strictPort: true,
-        hmr: false, // ✅ disables hot reload on production
-    },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  base: command === 'build' ? '/build/' : '',
+  build: {
+    outDir: 'public/build',
+    manifest: true,
+    emptyOutDir: true,
+  },
 }));
